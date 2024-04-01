ca46baa2327591238251184258c2033d72bda3d5
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-02 00:27:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Day 29 Challenge: Start Coding! 🚀
+// Question 85: 
+function position(str) {
+    return str.indexOf("code");
+}
+console.log(position("Learn coding"));
+// Question 86: 
+function checkingText(str) {
+    return str.includes("javeScript");
+}
+console.log(checkingText("learn Coding in javaScript"));
+// Question 87: 
+function extractSubstring(str) {
+    return str.substring(0, 15);
+}
+console.log(extractSubstring("helloWorld!!"));
---
+++ index.ts
@@ -0,0 +1,22 @@
+// Day 29 Challenge: Start Coding! 🚀
+
+// Question 85: 
+
+
+function position (str:string) : number {
+    return str.indexOf("code")
+}
+console.log(position("Learn coding"));
+
+// Question 86: 
+function checkingText (str: string) :boolean{ 
+      return str.includes("javeScript")
+}
+
+console.log (checkingText("learn Coding in javaScript"))
+// Question 87: 
+ // this function return 10 characters
+function extractSubstring (str:string) : string {
+    return str.substring (0,15)   ; 
+}
+console.log(extractSubstring("helloWorld!!" ));
